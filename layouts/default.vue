bb79df18cebb864b6ddc67e0ef106d24dbb20f93
David Dume <dumedavid11@gmail.com>
2023-07-17 19:11:57 +0200

    chiamata api per ricerca film

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="flex justify-center mt-10">
-        <NuxtLink class="px-4 py-2 border rouned" to="/">Home Page</NuxtLink>
+        <NuxtLink class="px-4 py-2 border rounded" to="/">Home Page</NuxtLink>
     </nav>
     <main>
         <slot/>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,29 @@
 <template>
 
-    <div>Index</div>
+    <div class="flex flex-col py-10">
+        <div>
+            <h2 class="text-2xl font-bold text-center">Movies</h2>
+        </div>
+        <div class="flex justify-center items-center h-32">
+            <input class="px-2 py-1 border border-gray-800 rounded min-w-64" type="text" v-model="searchTerm" placeholder="Search...">
+        </div>
+        <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 self-center gap-x-10 gap-y-10 mb-10">
+            <div v-for="movie in data?.results">
+            {{ movie.title }}</div>
+        </div>
+    </div>
+
 </template>
 
-<script>
+<script setup lang="ts">
+
+import { APIResponse } from '~~/types/APIResponse';
+    const searchTerm = ref('');
+    
+    
+    const url = computed(() => {
+        return `api/movies/search?query=${searchTerm.value}`
+    })
+
+    const {data} = await useFetch<APIResponse>(url);
 </script>
